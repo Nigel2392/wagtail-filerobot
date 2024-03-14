807246c6251355ed34f03a62cb1119d2ac1986d3
Nigel van Keulen <nigel2392@gmail.com>
2024-03-14 23:52:57 +0100

    Added comments.

--- filerobot/widgets/obj/theme.py
+++ filerobot/widgets/obj/theme.py
@@ -5,34 +5,34 @@ from django.utils.deconstruct import deconstructible
 class Theme:
     def __init__(
             self,
-            bg_primary: str = None,
-            bg_primary_active: str = None,
-            bg_secondary: str = None,
-            accent_primary: str = None,
+            bg_primary:            str = None,
+            bg_primary_active:     str = None,
+            bg_secondary:          str = None,
+            accent_primary:        str = None,
             accent_primary_active: str = None,
-            icons_primary: str = None,
-            icons_secondary: str = None,
-            borders_primary: str = None,
-            borders_secondary: str = None,
-            borders_strong: str = None,
-            light_shadow: str = None,
-            warning: str = None,
-            font_family: str = None,
+            icons_primary:         str = None,
+            icons_secondary:       str = None,
+            borders_primary:       str = None,
+            borders_secondary:     str = None,
+            borders_strong:        str = None,
+            light_shadow:          str = None,
+            warning:               str = None,
+            font_family:           str = None,
         ) -> None:
         
-        self.bg_primary = bg_primary
-        self.bg_primary_active = bg_primary_active
-        self.bg_secondary = bg_secondary
-        self.accent_primary = accent_primary
+        self.bg_primary            = bg_primary
+        self.bg_primary_active     = bg_primary_active
+        self.bg_secondary          = bg_secondary
+        self.accent_primary        = accent_primary
         self.accent_primary_active = accent_primary_active
-        self.icons_primary = icons_primary
-        self.icons_secondary = icons_secondary
-        self.borders_primary = borders_primary
-        self.borders_secondary = borders_secondary
-        self.borders_strong = borders_strong
-        self.light_shadow = light_shadow
-        self.warning = warning
-        self.font_family = font_family
+        self.icons_primary         = icons_primary
+        self.icons_secondary       = icons_secondary
+        self.borders_primary       = borders_primary
+        self.borders_secondary     = borders_secondary
+        self.borders_strong        = borders_strong
+        self.light_shadow          = light_shadow
+        self.warning               = warning
+        self.font_family           = font_family
 
     def _json(self):
         palette = notNoneDict()
